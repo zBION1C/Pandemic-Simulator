9332b02522656f8908daf0351ff7df0a01e467d8
Kage17 <65666727+Kage17@users.noreply.github.com>
2020-05-21 18:07:08 +0200

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -7,7 +7,7 @@ public class Frame extends JFrame {
     private int borderX = 800;
     private int borderY = 800;
 
-    public Frame() {
+    public Frame(double p, double r, double c, double v, double i, double s, double l, double d) {
         setSize(borderX,borderY);
         JFrame frame = new JFrame();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
