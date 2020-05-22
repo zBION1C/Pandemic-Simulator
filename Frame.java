17063ab3d3a2c743869ff9602abc210fb7f648e4
Kage17 <65666727+Kage17@users.noreply.github.com>
2020-05-22 14:24:43 +0200

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -4,14 +4,14 @@ import java.awt.event.ActionListener;
 
 public class Frame extends JFrame {
 
-    private int borderX = 800;
-    private int borderY = 800;
+    private int borderX = 1800;
+    private int borderY = 900;
 
     public Frame(double p, double r, double c, double v, double i, double s, double l, double d) {
         setSize(borderX,borderY);
         JFrame frame = new JFrame();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(new Simulatore());
+        add(new Simulatore(p, r, c, v, i, s, l, d));
         setVisible(true);
         frame.setBackground(new Color(101,101,101));
     }
